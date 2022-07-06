56b31a909b8717dbbbc67ca6397ddaac72823c50
ndehaven <106265519+ndehaven@users.noreply.github.com>
2022-07-06 15:46:00 -0700

    Spaced out brackets

--- Blackboard.java
+++ Blackboard.java
@@ -1,33 +1,48 @@
 import java.util.ArrayList;
 import java.util.Observable;
 
-public class Blackboard extends Observable {
-  private ArrayList<Point> points;
-  private ArrayList<Line> lines;
-  private static Blackboard instance;
-  
-  public static Blackboard getInstance(){
-    if (instance == null)
-      instance = new Blackboard();
-    return instance;
-  }
-  
-  private Blackboard() {
-    points = new ArrayList<Point>();
-    lines = new ArrayList<Line>();
-  }
-  
-  public ArrayList<Point> getPoints(){return points;}
-  
-  public ArrayList<Line> getLines(){return lines;}
-  
-  public void addPoint(Point p) {
-    points.add(p);
-    setChanged();
-    notifyObservers();
-  }
-  
-  public void setLines(ArrayList<Line> lines){
-    this.lines = lines;
-  }
+
+public class BlackBoard extends Observable{
+
+	private ArrayList<Point> points;
+	private ArrayList<Line> lines;
+	private static BlackBoard instance; 
+	
+	public static BlackBoard getInstance() { 
+		if (instance == null) { 
+			instance = new BlackBoard(); 
+		}
+		return instance; 
+		
+	}
+	
+	private BlackBoard() { 
+		points = new ArrayList<Point>(); 
+		lines = new ArrayList<Line>(); 
+		
+	}
+	
+	public ArrayList<Point> getPoints() { 
+		return points; 
+		
+	}
+	
+	public ArrayList<Line> getLines() { 
+		return lines; 
+		
+	}
+	
+	public void addPoint(Point p) { 
+		points.add(p);
+		setChanged();
+		notifyObservers();
+		
+	}
+	
+	public void setLines(ArrayList<Line> lines) {
+		this.lines = lines;
+	}
+
+
+	
 }
