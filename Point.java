d7429dd7c5fc9be11a1090a4171042b948d2e368
OOlivas <99464313+OOlivas@users.noreply.github.com>
2022-07-06 13:30:06 -0700

    Create Point.java
    Added two constructors, set/set methods and an override for equals(). Still requires imports

---
+++ Point.java
@@ -0,0 +1,27 @@
+public class Point{
+  private int x;
+  private int y;
+  private Color color;
+  
+  public Point(int x, int y, Color color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+  
+  public Point (int x, int y) {this(x,y, Color.BLACK);}
+  
+  public int getX() {return x;}
+  
+  public int getY() {return y;}
+  
+  public Color getColor() {return color;}
+  
+  public void setColor(Color color) {this.color = color;}
+  
+  @Override
+  public boolean equals(object o) {
+    Point other = (Point) o;
+    return this.x == other.x && this.y == other.y;
+  }
+}
