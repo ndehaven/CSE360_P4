8ccf61334072f7e17eaaac838dc5fc275b3c01d7
OOlivas <99464313+OOlivas@users.noreply.github.com>
2022-07-06 12:55:11 -0700

    Create Blackboard.java
    Created Blackboard.java with private constructor, and set/get methods for points and lines.

---
+++ Blackboard.java
@@ -0,0 +1,30 @@
+public class Blackboard extends Observable {
+  private ArrayList<Point> points;
+  private ArrayList<Line> lines;
+  private static Blackboard instance;
+  
+  public static Blackboard getInstance(){
+    if (instance == null)
+      instance = new Blackboard();
+    return instance;
+  }
+  
+  private Blackboard() {
+    points = new ArrayList<Point>();
+    lines = new ArrayList<Line>();
+  }
+  
+  public ArrayList<Point> getPoints(){return points;}
+  
+  public ArrayList<Line> getLines(){return lines;}
+  
+  public void addPoint(Point p) {
+    points.add(P);
+    setChanged();
+    notifyObservers();
+  }
+  
+  public void setLines(ArrayList<Line> lines){
+    this.lines = lines;
+  }
+}
