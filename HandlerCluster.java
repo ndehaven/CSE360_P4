4895016254c85a6f5095a7238bac35a173960da0
Aash2101 <106265551+Aash2101@users.noreply.github.com>
2022-07-08 23:04:51 -0700

    Update HandlerCluster.java

--- HandlerCluster.java
+++ HandlerCluster.java
@@ -1,3 +1,10 @@
+
+/*
+Author: Aashritha Machiraju
+Class Description: This class calls KMeans and colors the dots.
+*/
+
+
 import java.util.Random;
 import java.awt.Color;
 import java.util.ArrayList;
@@ -26,28 +33,7 @@ public class HandlerCluster implements Observer {
 			}
 		}
 
-		// Circles parameters
-//		Point[] centeres = cluster.returnMean1();
-//		Point center0 = centeres[0];
-//		Point center1 = centeres[1];
-//		double[] radius = cluster.returnRadius();
-//		int radius_0 = (int)radius[0];
-//		int radius_1 = (int)radius[1];
-//
-//		// Draw the Circles
-//		ArrayList<Circle> circles = new ArrayList<Circle>();
-//		Circle circle0 = new Circle(center0, radius_0);
-//		Circle circle1 = new Circle(center1, radius_1);
-//		circles.add(circle0);
-//		circles.add(circle1);
-//		bb.setCircles(circles);
-
-
-		// Draw the lines
-		// Use the centers and draw a line in between them
-//		ArrayList<Line> lines = new ArrayList<Line>();
-//		lines.add(new Line(center0, center1));
-//		bb.setLines(lines);
+		
 
 	}
 
