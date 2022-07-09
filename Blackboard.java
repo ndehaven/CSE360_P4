e73cf5b3dd4b1284fff644b1f5ead58ce996278f
ndehaven <106265519+ndehaven@users.noreply.github.com>
2022-07-08 22:29:34 -0700

    Update Blackboard.java

--- Blackboard.java
+++ Blackboard.java
@@ -1,47 +1,58 @@
 import java.util.ArrayList;
 import java.util.Observable;
 
-public class BlackBoard extends Observable {
-    private ArrayList<Point> points;
-    private ArrayList<Line> lines;
-    private ArrayList<Circle> circles;
-    private static Blackboard instance;
 
-    public static BlackBoard getInstance() {
-        if (instance == null)
-            instance = new Blackboard();
-        return instance;
-    }
+@SuppressWarnings("deprecation")
+public class BlackBoard extends Observable{
 
-    private BlackBoard() {
-        points = new ArrayList<Point>();
-        lines = new ArrayList<Line>();
-    }
-
-    public ArrayList<Point> getPoints(){
-        return points;
-    }
-
-    public ArrayList<Line> getLines() {
-        return lines;
-    }
-
-    public void addPoint(Point p) {
-        points.add(p);
-        setChanged();
-        notifyObservers();
-    }
-    public void empty(){
+	private ArrayList<Point> points;
+	private ArrayList<Line> lines;
+	private ArrayList<Circle> circles;
+	private static BlackBoard instance; 
+	
+	public static BlackBoard getInstance() { 
+		if (instance == null) { 
+			instance = new BlackBoard(); 
+		}
+		return instance; 
+		
+	}
+	
+	private BlackBoard() { 
+		points = new ArrayList<Point>(); 
+		lines = new ArrayList<Line>(); 
+		
+	}
+	
+	public ArrayList<Point> getPoints() { 
+		return points; 
+		
+	}
+	
+	public ArrayList<Line> getLines() { 
+		return lines; 
+		
+	}
+	
+	public void addPoint(Point p) { 
+		points.add(p);
+		setChanged();
+		notifyObservers();
+		
+	}
+	
+	public void empty(){
 		points.clear();
 		lines.clear();
 		setChanged();
 		notifyObservers();
 	}
-
-    public void setLines(ArrayList<Line> lines) {
-        this.lines = lines;
-    }
-    //Returns String containing the X and Y values for all points on the screen. Each line is a point
+	
+	public void setLines(ArrayList<Line> lines) {
+		this.lines = lines;
+	}
+	
+	//Returns String containing the X and Y values for all points on the screen. Each line is a point
 	@Override
 	public String toString(){
         String s = "";
@@ -52,9 +63,12 @@ public class BlackBoard extends Observable {
 		}
 		return s;
 	}
-
-
-    public void setCircles(ArrayList<Circle> circles) {
-        this.circles = circles;
-    }
+	
+	public ArrayList<Circle> getCircles() { 
+		return circles; 
+		
+	}
+	public void setCircles(ArrayList<Circle> circles) {
+		this.circles = circles;
+	}
 }
