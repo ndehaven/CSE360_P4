b30bc567fa8f54eae88129b01c09fe38a8e362a6
OOlivas <99464313+OOlivas@users.noreply.github.com>
2022-07-08 23:30:50 -0700

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,18 +1,30 @@
 import java.awt.*;
-
-// This class controls the Points and allows the user to be able 
-// to set the location of the Point and the color attached to it 
+/**
+ * This class controls the Points and allows the user to be able 
+ * to set the location of the Point and the color attached to it
+ */
 public class Point{
   private int x;
   private int y;
   private Color color;
   
+/**
+* Constructor for Point class.
+* @param x x-coordinate of the point
+* @param y y-coordinate of the point
+* @param color paints the color of the Point
+*/
   public Point(int x, int y, Color color) {
     this.x = x;
     this.y = y;
     this.color = color;
   }
   
+/**
+* Constructor that only takes x and y values. Color is set to black.
+* @param x x-coordinate of the point
+* @param y y-coordinate of the point
+*/
   public Point (int x, int y) {this(x,y, Color.BLACK);}
   
   public int getX() {return x;}
