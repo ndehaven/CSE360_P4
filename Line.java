4c48bd862b5fb5c18c465a7765a4b164f5bc7652
ndehaven <106265519+ndehaven@users.noreply.github.com>
2022-07-08 23:21:58 -0700

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,19 +1,24 @@
+//This class controls the Line and allows the user to create the points that the line will be drawn through 
+
 public class Line {
 
-    private Point endPoint1;
-    private Point endPoint2;
-
-    public Line(Point endPoint1, Point endPoint2) {
-        this.endPoint1 = endPoint1;
-        this.endPoint2 = endPoint2;
-
-    }
-
-    public Point getEndPoint1() {
-        return this.endPoint1;
-    }
-
-    public Point getEndPoint2() {
-        return this.endPoint2;
-    }
+	private Point endPoint1; 
+	private Point endPoint2; 
+	
+	
+	public Line(Point endPoint1, Point endPoint2) { 
+		this.endPoint1 = endPoint1; 
+		this.endPoint2 = endPoint2; 
+		
+		
+		
+	}
+	
+	public Point getEndPoint1() { 
+		return this.endPoint1;
+	}
+	
+	public Point getEndPoint2() { 
+		return this.endPoint2; 
+	}
 }
